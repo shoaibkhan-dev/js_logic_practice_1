dfc21d7dcf3431ce31dd0883030205bac29e4055
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-25 19:23:12 +0500

    add work

--- app.js
+++ app.js
@@ -29,3 +29,14 @@ function reverse(rev){
 }
 
 console.log(reverse("shoaib"))
+
+
+function evenOdd(num){
+    if(num % 2 ===0){
+        console.log("even")
+    }else{
+        console.log("odd")
+    }
+}
+
+evenOdd(6)
