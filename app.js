6c45ca23a27b88155cb9c1bf8225c5ae4995c0b1
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-25 18:56:48 +0500

    add files

---
+++ app.js
@@ -0,0 +1,31 @@
+function checkEvenOdd(number){
+
+    if(number % 2 === 0){
+        console.log("even")
+    }else{
+        console.log("odd")
+    }
+}
+
+checkEvenOdd(1)
+checkEvenOdd(29+1*3+2_5+1)
+checkEvenOdd(15)
+checkEvenOdd(23)
+checkEvenOdd(12)
+checkEvenOdd(26)
+
+
+
+function reverse(str){
+    return str.split("").reverse().join("");
+}
+
+console.log(reverse("enalporea"))
+console.log(reverse("irfan"))
+
+
+function reverse(rev){
+    return rev.split("").reverse().join("")
+}
+
+console.log(reverse("shoaib"))
