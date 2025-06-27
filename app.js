dece22b35e74eba22328cde177d6329806831e48
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-27 21:02:49 +0500

    add work

--- app.js
+++ app.js
@@ -56,3 +56,16 @@ function evenOdd(num){
 
 evenOdd(7)
 
+class Person {
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    fullName(){
+       console.log(`hi my name is ${this.firstName} ${this.lastName}`)
+    }
+}
+let persson1 = new Person("shoaib", "khan");
+let persson2 = new Person("ishfaq", "khan");
+
+persson1.fullName()
